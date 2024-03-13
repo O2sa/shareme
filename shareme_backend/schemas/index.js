998c68b6cdac718ec14775849b53f417bc796475
o2sa <o2sama1@outlook.sa>
2024-03-13 06:41:01 +0300

    remove backend

--- shareme_backend/schemas/index.js
+++
@@ -1,7 +0,0 @@
-import user from './user'
-import postedBy from './postedBy'
-import pin from './pin'
-import save from './save'
-import comment from './comment'
-
-export const schemaTypes = [user, postedBy, comment, save, pin]
